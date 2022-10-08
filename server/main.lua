7c046f70eb58737946dfeeb303f2bd15233be562
P3-R4 <90536386+P3-R4@users.noreply.github.com>
2022-10-08 23:30:29 +0200

    Update main.lua
    Broken

--- server/main.lua
+++ server/main.lua
@@ -87,11 +87,11 @@ if Config.OxInventory then
 		exports.ox_inventory:RegisterStash('society_' .. k, 'society_' .. k, 50, 200000)
 		nmafija = nmafija + 1
 	end
-else
-	for k,v in pairs(Config.Mafije) do
-		TriggerEvent('esx_society:registerSociety', k, k, 'society_' .. k, 'society_'..k, 'society_'..k, {type = 'public'})
-		nmafija = nmafija + 1
-	end
+end
+
+for k,v in pairs(Config.Mafije) do
+	TriggerEvent('esx_society:registerSociety', k, k, 'society_' .. k, 'society_'..k, 'society_'..k, {type = 'public'})
+	nmafija = nmafija + 1
 end
 
 print('[^1esxbalkan_mafije^0]: Napravio tim ^5ESX-Balkan^0 | Ucitano ^4' .. nmafija .. '^0 mafia')
